47d1731c6334fe0986024b059bc49795b45d4085
David Andersen <git@davidandersen.us>
2018-11-01 09:57:17 -0700

    install each run

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,24 +3,23 @@ set -e
 
 env
 
-if [ ! -d $HOME/steamcmd ]; then
-  mkdir -p $HOME/steamcmd
-  cd $HOME/steamcmd
+if [ ! -d ${HOME}/steamcmd ]; then
+  mkdir -p ${HOME}/steamcmd
+  cd ${HOME}/steamcmd
   wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
   tar xf steamcmd_linux.tar.gz
   rm steamcmd_linux.tar.gz
-  cd $HOME
+  cd ${HOME}
 fi
 
-if [ -z "$BETA" ]; then
-  EXTRA_ARGS=-beta $BETA
+if [ -n "${BETA}" ]; then
+  EXTRA_ARGS=-beta ${BETA}
 fi
 
-$HOME/steamcmd/steamcmd.sh +login anonymous +force_install_dir $HOME +app_update 600760 $EXTRA_ARGS +quit
+${HOME}/steamcmd/steamcmd.sh +login anonymous +force_install_dir ${HOME} +app_update 600760 ${EXTRA_ARGS} +quit
 find . -depth -name ".svn" -type d -exec rm -r "{}" \;
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
 exec ${MODIFIED_STARTUP}
