f5d05520b8c2b7582d3db579e00d5293bdf78197
David Andersen <git@davidandersen.us>
2018-11-01 14:24:31 -0700

    removed redundant container home

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,17 @@ env
 id
 
 HOME=/home/container
-CONTAINER_HOME=/home/container
 
-if [ ! -d ${CONTAINER_HOME}/steamcmd ]; then
-  mkdir -p ${CONTAINER_HOME}/steamcmd
-  curl -s https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz | tar -xz -C ${CONTAINER_HOME}/steamcmd
+if [ ! -d ${HOME}/steamcmd ]; then
+  mkdir -p ${HOME}/steamcmd
+  curl -s https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz | tar -xz -C ${HOME}/steamcmd
 fi
 
 if [ -n "${BETA}" ]; then
   EXTRA_ARGS="-beta ${BETA}"
 fi
 
-${CONTAINER_HOME}/steamcmd/steamcmd.sh +login anonymous +force_install_dir ${CONTAINER_HOME} +app_update 600760 ${EXTRA_ARGS} validate +quit
+${HOME}/steamcmd/steamcmd.sh +login anonymous +force_install_dir ${HOME} +app_update 600760 ${EXTRA_ARGS} validate +quit
 #find . -depth -name ".svn" -type d -exec rm -r "{}" \;
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
