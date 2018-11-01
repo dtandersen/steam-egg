94d4c618f3955c0847fde0596800e21c2d3df30a
David Andersen <git@davidandersen.us>
2018-11-01 10:14:33 -0700

    set container home dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,24 @@
 set -e
 
 env
+pwd
 
-if [ ! -d ${HOME}/steamcmd ]; then
-  mkdir -p ${HOME}/steamcmd
-  cd ${HOME}/steamcmd
+export CONTAINER_HOME=/home/container
+
+if [ ! -d ${CONTAINER_HOME}/steamcmd ]; then
+  mkdir -p ${CONTAINER_HOME}/steamcmd
+  cd ${CONTAINER_HOME}/steamcmd
   wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
   tar xf steamcmd_linux.tar.gz
   rm steamcmd_linux.tar.gz
-  cd ${HOME}
+  cd ${CONTAINER_HOME}
 fi
 
 if [ -n "${BETA}" ]; then
   EXTRA_ARGS=-beta ${BETA}
 fi
 
-${HOME}/steamcmd/steamcmd.sh +login anonymous +force_install_dir ${HOME} +app_update 600760 ${EXTRA_ARGS} +quit
+${CONTAINER_HOME}/steamcmd/steamcmd.sh +login anonymous +force_install_dir ${CONTAINER_HOME} +app_update 600760 ${EXTRA_ARGS} +quit
 find . -depth -name ".svn" -type d -exec rm -r "{}" \;
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
